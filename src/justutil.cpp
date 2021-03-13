e10dab3249e44a562b9917e109a22a55c5639a11
ScriptOni <marcuwynu23@gmail.com>
2021-03-13 12:34:16 +0800

    add mk command in jpb command for creating package
    rename jarc command to jpb command.

--- src/justutil.cpp
+++ src/justutil.cpp
@@ -2,18 +2,18 @@
 int main(int argc, char const *argv[])
 {
 	std::cout << "JustUtils v0.0.1 beta" << std::endl;
-	std::cout << "jvi				java interpreter(java jdk)" << std::endl;
-	std::cout << "cci				c interpreter(required mingw)" << std::endl;
-	std::cout << "cppi				c++ interpreter(required mingw)" << std::endl;
+	std::cout << "jvi				java interpreter(java jdk)." << std::endl;
+	std::cout << "cci				c interpreter(required mingw)." << std::endl;
+	std::cout << "cppi				c++ interpreter(required mingw)." << std::endl;
 	std::cout << "wr				browse html file." << std::endl;
 	std::cout << "xr				open file explorer in current directory." << std::endl;
 	std::cout << "cf				create file." << std::endl;
 	std::cout << "ctp				create projects." << std::endl;
+	std::cout << "jpb				java project builder." << std::endl;
 	std::cout << "ls				listing files." << std::endl;
 	std::cout << "mv				moving a file." << std::endl;
 	std::cout << "rm				remove a file." << std::endl;
 	std::cout << "fstr				find string in a files." << std::endl;
-	
 	system("pause > nul");
 	return 0;
 } 
