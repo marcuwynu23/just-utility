eb85717a318a22b3a0c2ebf9b6e6f7bc9ff7f6d3
PeculiarProgrammer <marcuwynu23@gmail.com>
2021-09-07 19:37:24 +0800

    added nodejs-web-app in ctp command

--- dist/test.sql
+++ dist/test.sql
@@ -1,5 +1,22 @@
-CREATE TABLE USER(
+.mode column
+.header ON
+
+CREATE TABLE IF NOT EXISTS USER(
     id INT PRIMARY KEY,
     name VARCHAR(20),
     age INT
 );
+
+
+.schema 
+-- INSERT INTO USER VALUES(1,"Mark Wayne B. Menorca",23);
+-- INSERT INTO USER VALUES(2,"Mark Wayne B. Menorca",23);
+-- INSERT INTO USER VALUES(3,"Mark Wayne B. Menorca",23);
+-- INSERT INTO USER VALUES(4,"Mark Wayne B. Menorca",23);
+-- INSERT INTO USER VALUES(5,"Mark Wayne B. Menorca",23);
+-- INSERT INTO USER VALUES(6,"Mark Wayne B. Menorca",23);
+-- INSERT INTO USER VALUES(7,"Mark Wayne B. Menorca",23);
+-- INSERT INTO USER VALUES(8,"Mark Wayne B. Menorca",23);
+
+
+SELECT * FROM USER;
