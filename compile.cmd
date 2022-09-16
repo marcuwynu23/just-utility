ab5dc3778d8460578ac9cd9b23bb945990d7157a
PeculiarProgrammer <marcuwynu23@gmail.com>
2022-09-16 18:36:38 +0800

    Create and Update: create chrome-dev and ct command;  update justutil command

--- compile.cmd
+++ compile.cmd
@@ -1,7 +1,7 @@
 @echo off
-set bins=(pyr,jr,justutil,sqlt3,wif,lc,fp,cat,dt,cppi,jvi,csi,cci,snip,jpb,ctp,cf,ls,wr,xr,fstr,rm,mv)
+set bins=(ct,chrome-dev,pyr,jr,justutil,sqlt3,wif,lc,fp,cat,dt,cppi,jvi,csi,cci,snip,jpb,ctp,cf,ls,wr,xr,fstr,rm,mv)
 for %%i in %bins% do (
 	echo compiling: %%i.cpp
-	g++  -std=gnu++11  -I "src\include"  -o dist\%%i src\%%i.cpp src\runner.cpp -lshlwapi
+	g++  -std=c++17  -I "src\include"  -o dist\%%i src\%%i.cpp src\runner.cpp -lshlwapi
 )
 echo compiling done..
