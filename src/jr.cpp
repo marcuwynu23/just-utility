17a2c9a0cd897b98537efb02f9c746d8839bed7f
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-16 20:25:13 +0800

    feat(jr): add --help flag and documentation page

--- src/jr.cpp
+++ src/jr.cpp
@@ -4,7 +4,39 @@
  */
 #include "runner.h"
 
+static void showHelp() {
+    cout << "NAME" << endl;
+    cout << "     jr - Run JAR files from C:/jar/" << endl;
+    cout << endl;
+    cout << "SYNOPSIS" << endl;
+    cout << "     jr [--help | -h] <file-name> [args]" << endl;
+    cout << endl;
+    cout << "DESCRIPTION" << endl;
+    cout << "     Runs a JAR file located in the C:/jar/ directory." << endl;
+    cout << "     The .jar extension is appended automatically." << endl;
+    cout << "     Optional arguments are passed to the JAR at runtime." << endl;
+    cout << endl;
+    cout << "OPTIONS" << endl;
+    cout << "     --help, -h  Show this help message and exit" << endl;
+    cout << endl;
+    cout << "ARGUMENTS" << endl;
+    cout << "     <file-name>  Name of the JAR file (without .jar extension)" << endl;
+    cout << "     [args]       Optional arguments passed to the JAR" << endl;
+    cout << endl;
+    cout << "DEPENDENCIES" << endl;
+    cout << "     java (Java Runtime Environment)" << endl;
+    cout << endl;
+    cout << "EXAMPLES" << endl;
+    cout << "     jr myapp               Run C:/jar/myapp.jar" << endl;
+    cout << "     jr myapp --verbose     Run with arguments" << endl;
+    cout << "     jr --help              Show this help" << endl;
+}
+
 int main(int argc, char const *argv[]){
+    if (argc > 1) {
+        string a = argv[1];
+        if (a == "--help" || a == "-h") { showHelp(); return 0; }
+    }
     if(argv[1] != NULL){
         string arg1,arg2,default_path,cmd;
 
@@ -15,7 +47,6 @@ int main(int argc, char const *argv[]){
         cmd = "java -jar  "+default_path+arg1+".jar"+" "+arg2;
         peculiar::runExe(cmd);
     }else{
-        peculiar::print("run command (with .jar extension).");
-        peculiar::print("jr <file-name>");
+        showHelp();
     }
 }
