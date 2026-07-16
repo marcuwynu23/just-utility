d6541b767fa99940fd60825505635b05dfb3d5e7
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-16 20:25:12 +0800

    feat(fp): add --help flag and documentation page

--- src/fp.cpp
+++ src/fp.cpp
@@ -1,17 +1,44 @@
-
 #include "runner.h"
+
+static void showHelp() {
+    cout << "NAME" << endl;
+    cout << "     fp - Display file properties" << endl;
+    cout << endl;
+    cout << "SYNOPSIS" << endl;
+    cout << "     fp [--help | -h] <file>" << endl;
+    cout << endl;
+    cout << "DESCRIPTION" << endl;
+    cout << "     Displays detailed properties and layout information" << endl;
+    cout << "     for the specified file using Windows fsutil." << endl;
+    cout << endl;
+    cout << "OPTIONS" << endl;
+    cout << "     --help, -h  Show this help message and exit" << endl;
+    cout << endl;
+    cout << "ARGUMENTS" << endl;
+    cout << "     <file>  Path to the file" << endl;
+    cout << endl;
+    cout << "DEPENDENCIES" << endl;
+    cout << "     fsutil (Windows built-in utility)" << endl;
+    cout << endl;
+    cout << "EXAMPLES" << endl;
+    cout << "     fp main.cpp          Show properties of main.cpp" << endl;
+    cout << "     fp C:\\file.txt       Show properties of file.txt" << endl;
+    cout << "     fp --help            Show this help" << endl;
+}
 
 int main(int argc, char const *argv[])
 {
- 	// creating source file
-	if(argv[1] != NULL){
+	if (argc > 1) {
+		string a = argv[1];
+		if (a == "--help" || a == "-h") { showHelp(); return 0; }
+	}
+ 	if(argv[1] != NULL){
 		string arg1,cmd;
 		arg1 = argv[1];
 		cmd = "fsutil file layout "+ arg1;
 		peculiar::runExe(cmd);
 	}else{
-		peculiar::print("display file properties.");
-		peculiar::print("   fp <file>");
+		showHelp();
 	}
 	return 0;
 } 
