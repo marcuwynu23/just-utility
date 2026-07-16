c9cba49ae251389f612a2b764b213748f41abef2
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-16 20:25:27 +0800

    feat(snip): add --help flag and documentation page

--- src/snip.cpp
+++ src/snip.cpp
@@ -1,3 +1,31 @@
 #include "runner.h"
 
-int main(){	peculiar::runExe("snippingtool");}
+static void showHelp() {
+    cout << "NAME" << endl;
+    cout << "     snip - Open Windows Snipping Tool" << endl;
+    cout << endl;
+    cout << "SYNOPSIS" << endl;
+    cout << "     snip [--help | -h]" << endl;
+    cout << endl;
+    cout << "DESCRIPTION" << endl;
+    cout << "     Opens the Windows Snipping Tool for taking screenshots." << endl;
+    cout << endl;
+    cout << "OPTIONS" << endl;
+    cout << "     --help, -h  Show this help message and exit" << endl;
+    cout << endl;
+    cout << "DEPENDENCIES" << endl;
+    cout << "     Snipping Tool (built-in Windows utility)" << endl;
+    cout << endl;
+    cout << "EXAMPLES" << endl;
+    cout << "     snip                Open Snipping Tool" << endl;
+    cout << "     snip --help         Show this help" << endl;
+}
+
+int main(int argc, char const *argv[]) {
+	if (argc > 1) {
+		string a = argv[1];
+		if (a == "--help" || a == "-h") { showHelp(); return 0; }
+	}
+	peculiar::runExe("snippingtool");
+	return 0;
+}
