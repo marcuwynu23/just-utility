1a407bcc19bbe95ed5cf7bc20278f1f5a05166c7
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-16 20:25:05 +0800

    feat(ct): add --help flag and documentation page

--- src/ct.cpp
+++ src/ct.cpp
@@ -4,17 +4,35 @@
  */
 #include "runner.h"
 
-int main(int argc, char const *argv[]) {
-  // clone current  instance of terminal.
-  if (argv[1] != NULL) {
-    string arg1;
-    arg1 = argv[1];
-    if (arg1 == "-h") {
-      peculiar::print("clone current  instance of terminal.");
-    } else {
-      peculiar::print("clone current  instance of terminal.");
-    }
+static void showHelp() {
+    cout << "NAME" << endl;
+    cout << "     ct - Clone the current terminal instance" << endl;
+    cout << endl;
+    cout << "SYNOPSIS" << endl;
+    cout << "     ct [--help | -h]" << endl;
+    cout << endl;
+    cout << "DESCRIPTION" << endl;
+    cout << "     Opens a new Windows Command Prompt window. Effectively" << endl;
+    cout << "     clones the current terminal instance." << endl;
+    cout << endl;
+    cout << "OPTIONS" << endl;
+    cout << "     --help, -h  Show this help message and exit" << endl;
+    cout << endl;
+    cout << "DEPENDENCIES" << endl;
+    cout << "     None (uses built-in Windows 'start' command)" << endl;
+    cout << endl;
+    cout << "EXAMPLES" << endl;
+    cout << "     ct                 Open a new terminal window" << endl;
+    cout << "     ct --help          Show this help" << endl;
+}
 
+int main(int argc, char const *argv[]) {
+  if (argc > 1) {
+    string a = argv[1];
+    if (a == "--help" || a == "-h") { showHelp(); return 0; }
+  }
+  if (argv[1] != NULL) {
+    showHelp();
   } else {
     string cmd;
     cmd = "start cmd";
