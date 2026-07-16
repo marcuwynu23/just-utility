f59b6e696c2d586c3898b13c6d3d78345586236f
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-16 20:25:27 +0800

    feat(rm): add --help flag and documentation page

--- src/rm.cpp
+++ src/rm.cpp
@@ -1,8 +1,37 @@
-
 #include "runner.h"
+
+static void showHelp() {
+    cout << "NAME" << endl;
+    cout << "     rm - Remove (delete) a file" << endl;
+    cout << endl;
+    cout << "SYNOPSIS" << endl;
+    cout << "     rm [--help | -h] <file>" << endl;
+    cout << endl;
+    cout << "DESCRIPTION" << endl;
+    cout << "     Deletes the specified file from the filesystem." << endl;
+    cout << "     Uses the Windows 'del' command internally." << endl;
+    cout << endl;
+    cout << "OPTIONS" << endl;
+    cout << "     --help, -h  Show this help message and exit" << endl;
+    cout << endl;
+    cout << "ARGUMENTS" << endl;
+    cout << "     <file>  Path to the file to delete" << endl;
+    cout << endl;
+    cout << "DEPENDENCIES" << endl;
+    cout << "     None (uses built-in Windows commands)" << endl;
+    cout << endl;
+    cout << "EXAMPLES" << endl;
+    cout << "     rm temp.txt            Delete temp.txt" << endl;
+    cout << "     rm ..\\old.log          Delete file in parent directory" << endl;
+    cout << "     rm --help              Show this help" << endl;
+}
 
 int main(int argc, char const *argv[])
 {
+	if (argc > 1) {
+		string a = argv[1];
+		if (a == "--help" || a == "-h") { showHelp(); return 0; }
+	}
 	string arg1,cmd;
 
 	if(argv[1] != NULL){
@@ -10,9 +39,8 @@ int main(int argc, char const *argv[])
 		cmd = "del "+ arg1;
 		peculiar::runExe(cmd);
 	}else{
-		peculiar::print("remove a file.");
-		peculiar::print("<rm> <file>");
+		showHelp();
 	}
 	return 0;
 } 
- 
+  
