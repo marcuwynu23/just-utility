19f64dfb08611685a7dd54fd4fbe78b4d65382fd
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-16 20:25:05 +0800

    feat(csi): add --help flag and documentation page

--- src/csi.cpp
+++ src/csi.cpp
@@ -7,12 +7,42 @@
 #include <ctime>
 #include "runner.h"
 
+static void showHelp() {
+    cout << "NAME" << endl;
+    cout << "     csi - C# source code interpreter (compile + run)" << endl;
+    cout << endl;
+    cout << "SYNOPSIS" << endl;
+    cout << "     csi [--help | -h] <sourcefile>" << endl;
+    cout << endl;
+    cout << "DESCRIPTION" << endl;
+    cout << "     Compiles and runs a C# source file in a single step." << endl;
+    cout << "     The source file is compiled with the C# compiler (csc), executed," << endl;
+    cout << "     and the resulting binary is cleaned up automatically." << endl;
+    cout << "     Shows compile time and execution time after running." << endl;
+    cout << endl;
+    cout << "OPTIONS" << endl;
+    cout << "     --help, -h  Show this help message and exit" << endl;
+    cout << endl;
+    cout << "ARGUMENTS" << endl;
+    cout << "     <sourcefile>  Name of the C# source file (without .cs extension)" << endl;
+    cout << endl;
+    cout << "DEPENDENCIES" << endl;
+    cout << "     csc (C# compiler, part of .NET Framework SDK / Roslyn)" << endl;
+    cout << endl;
+    cout << "EXAMPLES" << endl;
+    cout << "     csi program         Compile and run program.cs" << endl;
+    cout << "     csi --help          Show this help" << endl;
+}
+
 int main(int argc, char const *argv[])
 {
+	if (argc > 1) {
+		string a = argv[1];
+		if (a == "--help" || a == "-h") { showHelp(); return 0; }
+	}
 	string arg1,arg2;
 	string cmd1,cmd2,cmd3;
 
-	/*interpret cpp source*/
 	if (argv[1] !=NULL){
 		
 		arg1 = argv[1];
@@ -36,8 +66,7 @@ int main(int argc, char const *argv[])
 		cout << "running time: " << running_time.count() <<"s" << endl;
 	
 	}else{
-		peculiar::print("interpret c# source file.");
-		peculiar::print("   csi <sourcefile name> ");
+		showHelp();
 	}
 	return 0;
 }
